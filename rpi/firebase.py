94d8d6c7ea703c7ce983eef0e03a6aaf49e1e8ea
Ivan Ivanov <ivancivanov@outlook.com>
2021-06-17 19:36:31 +0300

    implement firebase logic

--- rpi/firebase.py
+++ rpi/firebase.py
@@ -1,4 +1,24 @@
-# code for Google
+import firebase_admin
+from datetime import datetime
+
+from firebase_admin import db, credentials
+
+cert = credentials.Certificate('./cert.json')
+app = firebase_admin.initialize_app(cert, {
+    "databaseURL": "https://csyntax-weather-station-default-rtdb.firebaseio.com"
+})
+
+ref = db.reference('/')
 
 def push_to_firebase(data):
     (temp, press, hum) = data
+    
+    now = datetime.now().replace(microsecond=0).isoformat()
+
+    ref.child(now).set({
+        'Temperature': temp,
+        'Pressure': press,
+        'Humidity': hum,
+    })
+
+    print('Success push to Google Firebase')
