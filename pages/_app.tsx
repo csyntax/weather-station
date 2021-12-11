9718c496d112339895c542333bf7738a4af7fd8e
Ivan Ivanov <csyntax@outlook.com>
2021-12-11 19:19:39 +0200

    init next app

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from 'next/app'
+import SSRProvider from 'react-bootstrap/SSRProvider';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function MyApp(props: AppProps) {
+    const { Component, pageProps } = props;
+
+
+    return(
+        <SSRProvider>
+            <Component {...pageProps} />
+        </SSRProvider>
+    );
+}
